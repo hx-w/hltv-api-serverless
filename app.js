25c56115d45650185d7ac4a083dfa448d8c62f09
hx-w <herixth@163.com>
2021-12-24 20:10:00 +0800

    add tencent serverless

---
+++ app.js
@@ -0,0 +1,38 @@
+const HLTV = require('hltv-api').default
+const express = require('express')
+const app = express()
+
+app.get('/news', async (req, res) => {
+    res.json(await HLTV.getNews())
+})
+
+app.get('/matches', async (req, res) => {
+    res.json(await HLTV.getMatches())
+})
+
+app.get('/results', async (req, res) => {
+    res.json(await HLTV.getResults())
+})
+
+app.get('/players', async (req, res) => {
+    res.json(await HLTV.getTopPlayers())
+})
+
+app.get('/teams', async (req, res) => {
+    res.json(await HLTV.getTopTeams())
+})
+
+app.get('/player', async (req, res) => {
+    res.json(await HLTV.getPlayerById(req.params().playerId))
+})
+
+
+// Error handler
+app.use(function (err, req, res, next) {
+    console.error(err)
+    res.status(403).send({'detail': `server error ${err}`})
+})
+
+app.listen(9000, () => {
+    console.log(`Server start on http://localhost:9000`);
+})
