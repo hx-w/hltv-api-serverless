a1f300b7eb9509d225d68ed67b1f7ba202287335
hx-w <herixth@163.com>
2022-01-24 10:17:24 +0800

    new api test

--- new_serverless/news.js
+++ new_serverless/news.js
@@ -1,19 +1,11 @@
 const HLTV = require('hltv')
 
-exports.handler = async function(event, context) {
-    try {
-        const news = await HLTV.getNews()
+exports.handler = async function (event, context) {
+    HLTV.getNews().then((resp) => {
         return {
             statusCode: 200,
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(news)
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(resp)
         }
-    }
-    catch(err) {
-        return {
-            statusCode: 403,
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({'detail': `${err}`})
-        }
-    }
+    })
 }
