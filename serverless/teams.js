6f6ff3531d4476402fa11c36f3c341d56fcfd69d
hx-w <herixth@163.com>
2022-04-07 22:24:19 +0800

    await

--- serverless/teams.js
+++ serverless/teams.js
@@ -1,11 +1,11 @@
 const HLTV = require('hltv').default
 
 exports.handler = async function (event, context) {
-    await HLTV.getTeamRanking().then(res => {
-        return {
-            statusCode: 200,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(res)
-        }
-    })
+    const info = await HLTV.getTeamRanking()
+    console.log(info)
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(info)
+    }
 }
