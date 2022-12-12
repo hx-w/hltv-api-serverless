611078596f57e985e390dfe744e5439ab7ab11e1
CarOL <herixth@163.com>
2022-12-12 13:11:25 +0800

    update stats format

--- serverless/stats.js
+++ serverless/stats.js
@@ -2,10 +2,8 @@ const HLTV = require('hltv-api').default
 
 exports.handler = async function (event, context) {
     try {
-        const _id = event['queryStringParameters']['id']
-        const info = await HLTV.getMatchById({
-          'id': _id
-        })
+        const _id = event['queryStringParameters']['matchId']
+        const info = await HLTV.getMatchById(_id)
         return {
             statusCode: 200,
             headers: { "Content-Type": "application/json" },
